54bf68898a7bce67636c9e1d656b57164833574c
Yogi_Hariyani <hariyaniyogi7292@gmail.com>
2022-11-28 15:53:04 +0530

    add root route in web.php

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 */
+Route::get("/", function(){
+    return View::make('welcome');
+});
